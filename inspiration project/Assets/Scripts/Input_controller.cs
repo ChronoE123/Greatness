ae74ca7051dbf8c6a91316ad5c70ee12882b9281
elliott <e.desouza1996@hotmail.com>
2021-10-13 20:55:09 +0100

    just the start

---
+++ inspiration project/Assets/Scripts/Input_controller.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[Serializable]
+
+public class MoveInputEvent : UnityEvent<float,float> { }
+
+public class Input_controller : MonoBehaviour
+{
+
+
+    Controls controls;
+    public MoveInputEvent moveInputEvent;
+
+    public void Awake()
+    {
+        controls = new Controls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+        controls.Gameplay.Move.performed += OnMovePerformed;
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        moveInputEvent.Invoke(moveInput.x, moveInput.y);
+        Debug.Log($"move input: { moveInput}");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
