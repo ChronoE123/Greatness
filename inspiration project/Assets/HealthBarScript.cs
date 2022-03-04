245eea1f7bb012470cf77cb00c41271554cdfe34
elliott <e.desouza1996@hotmail.com>
2022-03-04 15:05:57 +0000

    adding health bar

---
+++ inspiration project/Assets/HealthBarScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxvalue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health; 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
